6b324e6370db8345277fd87e9d509a9d50586f61
Serhii Izotov <sergiyizotov@gmail.com>
2023-01-27 15:26:08 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,66 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const gallery = document.querySelector('.gallery');
+const items = []
 
-console.log(galleryItems);
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement('div')
+    galleryItem.className = 'gallery__item'
+    const galleryLink =document.createElement('a')
+    galleryLink.className = 'gallery__link'
+    galleryLink.href = element.original
+const galleryImage = document.createElement ('img')
+galleryImage.className ='gallery__image'
+galleryImage.src = element.preview
+galleryImage.setAttribute('data-source', element.original)
+galleryImage.alt = element.description
+
+galleryItem.append(galleryLink)
+galleryLink.append(galleryImage)
+items.push(galleryItem)
+})
+
+gallery.append(...items)
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`<img src="${selectedImage}" width="800" height="600">`)
+    {
+        onShow: () => {
+            document.addEventListener('keydown', onEscapeButton);
+        }
+        onClose: () => {
+            document.removeEventListener('keydown', onEscapeButton);
+        }
+    };
+
+    instance.show()
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt'
+       
+    } );
+
+    function onEscapeButton(evt) {
+        if (evt.key === 'Escape') {
+            instance.close();
+        }
+    }
+
+    // document.addEventListener('keydown', e => {
+    //     if (e.key === 'Escape') {
+    //         instance.close()
+    //     }
+    // })
+})
+
+
